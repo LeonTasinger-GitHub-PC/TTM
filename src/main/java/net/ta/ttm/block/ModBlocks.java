1ec3469e03366b1a2ea355f341372ec0bd5485fe
LeonTasinger-GitHub-PC <134704665+LeonTasinger-GitHub-PC@users.noreply.github.com>
2025-11-16 21:45:28 +0700

    Hardened Obsidian Block and ...

--- src/main/java/net/ta/ttm/block/ModBlocks.java
+++ src/main/java/net/ta/ttm/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.ta.ttm.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -13,9 +15,16 @@ import net.ta.ttm.TTM;
 public class ModBlocks {
     public static final Block OBSIDIANITE_BLOCK = registerBlock("obsidianite_block",
             new Block(AbstractBlock.Settings.create()
-                    .strength(10f)
+                    .strength(25f, 12f)
                     .requiresTool()
                     .sounds(BlockSoundGroup.METAL)
+            ));
+
+    public static final Block RAW_OBSIDIANITE_BLOCK = registerBlock("raw_obsidianite_block",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(12f, 6f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE)
             ));
 
     private static Block registerBlock(String name, Block block){
@@ -30,5 +39,9 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         TTM.LOGGER.info("Registering Mod Blocks for " + TTM.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.OBSIDIANITE_BLOCK);
+            entries.add(ModBlocks.RAW_OBSIDIANITE_BLOCK);
+        });
     }
 }
