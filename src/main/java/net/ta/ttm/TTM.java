5eb68134db09eb52f627ce729b47a389dc229b6f
LeonTasinger-GitHub-PC <134704665+LeonTasinger-GitHub-PC@users.noreply.github.com>
2025-11-16 11:10:52 +0700

    Obsidianite!

--- src/main/java/net/ta/ttm/TTM.java
+++ src/main/java/net/ta/ttm/TTM.java
@@ -2,6 +2,7 @@ package net.ta.ttm;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ta.ttm.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TTM implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/ta/ttm/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.ta.ttm.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.ta.ttm.TTM;
+
+public class ModItems {
+    public static final Item OBSIDIANITE = registerItem("obsidianite", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TTM.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        TTM.LOGGER.info("Registering Mod Items for" + TTM.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(OBSIDIANITE);
+        });
+    }
+}
