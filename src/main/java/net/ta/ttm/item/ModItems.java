c159ddf5dd6386f72256824f5a4c98f9840e087f
LeonTasinger-GitHub-PC <134704665+LeonTasinger-GitHub-PC@users.noreply.github.com>
2025-11-20 16:03:11 +0700

    Tooltip

--- src/main/java/net/ta/ttm/item/ModItems.java
+++ src/main/java/net/ta/ttm/item/ModItems.java
@@ -3,17 +3,28 @@ package net.ta.ttm.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.ta.ttm.TTM;
 import net.ta.ttm.item.custom.WrenchItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item OBSIDIANITE = registerItem("obsidianite", new Item(new Item.Settings()));
     public static final Item RAW_OBSIDIANITE = registerItem("raw_obsidianite", new Item(new Item.Settings()));
     public static final Item WRENCH = registerItem("wrench", new WrenchItem(new Item.Settings()));
-    public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFLOWER)));
+    public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFLOWER)){
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.ttm.cauliflower.tooltip"));
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(TTM.MOD_ID, name), item);
--- src/main/java/net/ta/ttm/item/custom/WrenchItem.java
+++ src/main/java/net/ta/ttm/item/custom/WrenchItem.java
@@ -2,24 +2,29 @@ package net.ta.ttm.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 import net.ta.ttm.block.ModBlocks;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
 
 public class WrenchItem extends Item {
     private static final Map<Block, Block> WRENCH_MAP =
             Map.of(
-                ModBlocks.OBSIDIANITE_BLOCK, Blocks.AIR
+                ModBlocks.OBSIDIANITE_BLOCK, Blocks.AIR,
+                ModBlocks.MAGIC_BLOCK, Blocks.AIR
             );
 
     public WrenchItem(Settings settings) {
@@ -53,5 +58,16 @@ public class WrenchItem extends Item {
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.translatable("tooltip.ttm.wrench.shift"));
+        }else{
+            tooltip.add(Text.translatable("tooltip.ttm.wrench.no_shift"));
+        }
+
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
